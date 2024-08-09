493ba7ab6cadeeda36cfe4f125e53fca1990eaa2
Stokn9x <capper2704@hotmail.dk>
2024-08-09 23:52:05 +0200

    Another fix to the css

--- src/Cards/PlayerCard.jsx
+++ src/Cards/PlayerCard.jsx
@@ -4,7 +4,9 @@ import './../Css/PlayerCard.css';
 function PlayerCard({ player }) {
     return (
         <div className="playerCard">
-            <img className="Card-img" src={player.profilePic} alt="profile picture"></img>
+            <div className="img-container">
+                <img className="Card-img" src={player.profilePic} alt="profile picture"></img>
+            </div>
             <h2>{player.username}</h2>
             <p>kd: {player.kd}</p>
             <p>kast: {player.kast}</p>
