66ea8383a7f5be79709371b5356a5ef29fd413a4
Casper Jensen <capper2704@hotmail.dk>
2024-08-10 23:25:16 +0200

    Small cahnges

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -3,22 +3,23 @@ import { Link } from 'react-router-dom';
 import '/src/Css/FrontPage.css';
 
 const Homepage = () => {
-    return (
-    
-      <body>
-            <header>
-                {/*<img className="logo" src="Images/AogC.jpg" alt="logo"></img>*/}
-                <nav>
-                    <ul className="nav_links">
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Random</a></li>
-                        <li><a href="/login">Login</a></li>
-                    </ul>             
-                </nav>
-                <a className="cta" href="#"><button>Contact</button></a>          
-            </header>
-      </body>  
-    );
+	return (
+
+		<div>
+			<p className="">Homepage</p>
+			{/*<img className="logo" src="Images/AogC.jpg" alt="logo"></img>*/}
+			<nav>
+				<ul className="nav_links">
+					<li><a href="#">About us</a></li>
+					<li><a href="#">Random</a></li>
+					<li><a href="/login">Login</a></li>
+				</ul>
+			</nav>
+			<a className="cta" href="#"><button>Contact</button></a>
+		</div>
+
+
+	);
 };
 
 export default Homepage;
