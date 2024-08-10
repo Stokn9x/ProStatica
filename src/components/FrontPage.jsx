ea8af67ace49af505fb41069132b0be911186837
Casper Jensen <capper2704@hotmail.dk>
2024-08-10 14:57:51 +0200

    Removed Alissa's Purple color

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
                     <ul className="nav_links">
                         <li><a href="#">About us</a></li>
                         <li><a href="#">Random</a></li>
-                        <li><a href="#">Login</a></li>
+                        <li><a href="/login">Login</a></li>
                     </ul>             
                 </nav>
                 <a className="cta" href="#"><button>Contact</button></a>          
