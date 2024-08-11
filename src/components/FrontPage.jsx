d49a1838b7b0e9fcb0b9ade1df11307691440c6c
Alissa Andrea Storm <alissaalidib@hotmail.com>
2024-08-11 12:44:19 +0200

    Changes to the frontpage -> Navbar

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '/src/Css/FrontPage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Homepage = () => {
-	return (
-
-		<div>
-			<p className="">Homepage</p>
-			{/*<img className="logo" src="Images/AogC.jpg" alt="logo"></img>*/}
-			<nav>
-				<ul className="nav_links">
-					<li><a href="#">About us</a></li>
-					<li><a href="#">Random</a></li>
-					<li><a href="/login">Login</a></li>
-				</ul>
-			</nav>
-			<a className="cta" href="#"><button>Contact</button></a>
-		</div>
-
-
-	);
+    return (
+        <div>
+            <div className="navbar">
+                <nav>
+                    <ul className="navbar-list">
+                        <img src="your_logo_image_here" alt="Your Logo" />
+                        <li><Link to="/About">About</Link></li>
+                        <li><Link to="#">Random</Link></li>
+                        <li><Link to="/Login">Login</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 };
 
 export default Homepage;
