9e48437979c0d11fb6ab9378cd10e801a7b55051
Casper Jensen <capper2704@hotmail.dk>
2024-08-22 14:12:29 +0200

    Made some changes to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import '/src/Css/Header.css';
-import SearchField from './SearchFIeld';
-import ProfileMenu from './ProfileMenu';
+import SearchField from './SearchFIeld.jsx';
+import ProfileMenu from './ProfileMenu.jsx';
 
-function Header({ currentUser, handleLogout }){
-
-	return(
-		<div className="header">
-			<div>
-				<SearchField />
-				<ProfileMenu currentUser={currentUser} handleLogout={handleLogout} />
-			</div>
-		</div>
-
-	);
+function Header({ currentUser, handleLogout }) {
+    return (
+        <div className="header">
+            <SearchField />
+            <div className="icons">
+                <a href="#">
+                    <img src="C:/Users/cappe/Source/Repos/Esport_UI/src/assets/Icon/friend-icon.svg" alt="Friend Icon" />
+                </a>
+                <a href="#">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </div>
+            <ProfileMenu currentUser={currentUser} handleLogout={handleLogout} />
+        </div>
+    );
 }
 
-export default Header
+export default Header;
